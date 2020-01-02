a8d33ab159307c6572fb298aa15ca61b4580ebcf
jbtule <jay.tuley@ekonbenefits.com>
2020-01-02 16:58:55 -0600

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    for file in argv do
+        let input = File.ReadAllBytes(file);
+        let output = input |> Array.filter(fun b -> b <> 0x1Duy)
+        File.WriteAllBytes(file, output)
+
+    0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,2 @@
+# noz
+ remove Clipper End of File Characters
